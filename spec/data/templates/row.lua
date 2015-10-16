6802ca7c8c7f9d5afd01b976877a7b57aaf5b272
Thiago Bastos <tbastos@users.noreply.github.com>
2015-10-17 00:49:18 +0200

    Ignore last newline in template files. Fix AppVeyor.

--- spec/data/templates/row.lua
+++ spec/data/templates/row.lua
@@ -3,4 +3,4 @@
 {% elseif tp == 'string' then %}'{{v}}'
 {% elseif tp == 'number' then %}{{v}}
 {% elseif tp == 'boolean' then %}{{v and 'true' or 'false'}}
-{% else error('unsupported type') end %},
+{% else error('unsupported type') end %}
